35545241f76d8f319dbe23b23ab372c5e7fcdb06
Vladimir Bessonov <ciscapello@gmail.com>
2024-06-04 18:00:52 +0300

    add socket service

---
+++ socket-service/go.mod
@@ -0,0 +1,11 @@
+module github.com/ciscapello/socket-service
+
+go 1.22.3
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+)
---
+++ socket-service/internal/common/logger/logger.go
@@ -0,0 +1,65 @@
+package logger
+
+import (
+	"io"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/ciscapello/socket-service/internal/application/config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	logger *zap.Logger
+	once   sync.Once
+)
+
+func GetLogger(config *config.Config) *zap.Logger {
+	once.Do(func() {
+		logger = InitLogger(config.LogPath)
+	})
+	return logger
+}
+
+func createLogDirectoryIfNotExists(logPath string) {
+	if _, err := os.Stat(logPath); os.IsNotExist(err) {
+		err = os.Mkdir(logPath, 0777)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func getLogWriter(logPath string) zapcore.WriteSyncer {
+	file, err := os.OpenFile(logPath+"/app.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+
+	multiwriter := io.MultiWriter(file, os.Stdout)
+
+	if err != nil {
+		panic(err)
+	}
+	return zapcore.AddSync(multiwriter)
+}
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewDevelopmentEncoderConfig()
+	encoderConfig.EncodeTime = customTimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func customTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006/01/02 15:04:05.000"))
+}
+
+func InitLogger(logPath string) *zap.Logger {
+	createLogDirectoryIfNotExists(logPath)
+	writer := getLogWriter(logPath)
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writer, zapcore.DebugLevel)
+	logger := zap.New(core)
+
+	return logger
+}
